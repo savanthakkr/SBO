c5ad45dceeb78922df5257f95f94434dec93ac02
savanponda <70908030+savanponda@users.noreply.github.com>
2024-07-07 19:27:42 +0530

    final

--- config/database.js
+++ config/database.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 
 
 // Replace these values with your actual database credentials
-// const DB_NAME = 'sbo_new';
-// const DB_USER = 'admin';
-// const DB_PASSWORD = 'Savanponda1122';
-// const DB_HOST = 'sbo.cpaqskmk40f1.ap-south-1.rds.amazonaws.com';
+const DB_NAME = 'sbo_new';
+const DB_USER = 'admin';
+const DB_PASSWORD = 'Savanponda1122';
+const DB_HOST = 'sbo.cpaqskmk40f1.ap-south-1.rds.amazonaws.com';
 
-// // Set up the Sequelize instance
-// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-//   host: DB_HOST,
-//   dialect: 'mysql',
-// });
+// Set up the Sequelize instance
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+});
 
 // Replace these values with your actual database credentials
 // const DB_NAME = 'bmgf5jemd29cumrpew82';
@@ -26,14 +26,14 @@ require('dotenv').config();
 //   dialect: 'mysql',
 // });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME, 
+//   process.env.DB_USER, 
+//   process.env.DB_PASSWORD, 
+//   {
+//   host: process.env.DB_HOST,
+//   dialect: 'mysql',
+// });
 
 async function testConnection() {
   try {
